37ba8d1533f040ebef21fafdfc33e1af38d03880
mdabusayedislam <abusayediit@gmail.com>
2023-08-26 15:54:51 +0600

    Add project files.

---
+++ DemoLibrary/DataAccess/DemoDataAccess.cs
@@ -0,0 +1,30 @@
+﻿using DemoLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.DataAccess
+{
+    public class DemoDataAccess : IDataAccess
+    {
+        private List<PersonModel> people = new();
+        public DemoDataAccess()
+        {
+            people.Add(item: new PersonModel { Id = 1, FirstName = "Abu", LastName = "Sayed" });
+            people.Add(item: new PersonModel { Id = 2, FirstName = "Moinul", LastName = "Hossain" });
+        }
+        public List<PersonModel> GetPeople()
+        {
+            return people;
+        }
+        public PersonModel InsertPerson(string firstName, string lastName)
+        {
+            PersonModel person = new() { FirstName = firstName, LastName = lastName };
+            person.Id = people.Max(x => x.Id) + 1;
+            people.Add(person);
+            return person;
+        }
+    }
+}
---
+++ DemoLibrary/Handlers/GetPersonByIdHandler.cs
@@ -0,0 +1,28 @@
+﻿using DemoLibrary.Models;
+using DemoLibrary.Queries;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.Handlers
+{
+    public class GetPersonByIdHandler : IRequestHandler<GetPersonByIdQuery, PersonModel>
+    {
+        private readonly IMediator mediator;
+
+        public GetPersonByIdHandler(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+        public async Task<PersonModel> Handle(GetPersonByIdQuery request, CancellationToken cancellationToken)
+        {
+            var results= await mediator.Send(request:new GetPersonListQuery());
+            var output= results.FirstOrDefault(x=>x.Id==request.id);
+            return output;
+        }
+    }
+}
